3ea8d6103da7a4dff29bfac868ec74f4a6e5c97a
ishi12121 <isht1402@gmail.com>
2024-03-14 14:38:50 +0530

    build lint error of file upload fixed

--- discord-clone/components/file-upload.tsx
+++ discord-clone/components/file-upload.tsx
@@ -2,23 +2,33 @@
 
 import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
- 
+
 import { UploadDropzone } from "@/lib/uploadthing";
+
 import "@uploadthing/react/styles.css";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: "messageFile" | "serverImage";
+  endpoint: "messageFile" | "serverImage"
 }
 
-export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+export const FileUpload = ({
+  onChange,
+  value,
+  endpoint
+}: FileUploadProps) => {
   const fileType = value?.split(".").pop();
 
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
-        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
+        />
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
@@ -27,14 +37,14 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
           <X className="h-4 w-4" />
         </button>
       </div>
-    );
+    )
   }
 
   if (value && fileType === "pdf") {
     return (
       <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
-        <a
+        <a 
           href={value}
           target="_blank"
           rel="noopener noreferrer"
@@ -50,18 +60,18 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
           <X className="h-4 w-4" />
         </button>
       </div>
-    );
+    )
   }
 
   return (
     <UploadDropzone
       endpoint={endpoint}
-      onClientUploadComplete={(res: { url: string | undefined; }[]) => {
+      onClientUploadComplete={(res) => {
         onChange(res?.[0].url);
       }}
-      onUploadError={(error: any) => {
+      onUploadError={(error: Error) => {
         console.log(error);
       }}
     />
-  );
-};
+  )
+}
