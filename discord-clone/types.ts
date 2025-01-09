22516da7d266b0002dd2b65d51512436114257fa
ishi12121 <isht1402@gmail.com>
2025-01-09 17:32:33 +0530

    .

--- discord-clone/types.ts
+++ discord-clone/types.ts
@@ -1,7 +1,7 @@
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
-import { Server, Member, Profile } from "@prisma/client"
+import { Server, Member, Profile } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
@@ -14,3 +14,4 @@ export type NextApiResponseServerIo = NextApiResponse & {
     };
   };
 };
+declare module "uuid";
